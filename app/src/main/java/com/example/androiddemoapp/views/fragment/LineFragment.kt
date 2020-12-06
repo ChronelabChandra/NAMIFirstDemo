2ab81145a117b6bfad8eca3752bec6e2741c602b
Chandra Mohan Jayaswal <chandra.ets@gmail.com>
2020-12-06 08:02:20 +0545

    05-Dec-2020 Info file updated

--- app/src/main/java/com/example/androiddemoapp/views/fragment/LineFragment.kt
+++ app/src/main/java/com/example/androiddemoapp/views/fragment/LineFragment.kt
@@ -1,33 +1,64 @@
 package com.example.androiddemoapp.views.fragment
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.androiddemoapp.R
 import androidx.lifecycle.Observer
-import com.example.androiddemoapp.views.LineViewModel
 
 
 class LineFragment : Fragment() {
 
-    private lateinit var lineViewModel: LineViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        lineViewModel =
-            ViewModelProviders.of(this).get(LineViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_line, container, false)
-        val textView: TextView = root.findViewById(R.id.text_line)
-        lineViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_line)
+
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        drawLine()
+    }
+
+    private fun drawLine() {
+        val bitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(Color.RED)
+
+        // Holds the style and color informations about hot to draw geometries
+        val paint = Paint()
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 2f
+        paint.isAntiAlias = true
+
+        val offset = 50
+        canvas.drawLine(
+            offset.toFloat(),
+            canvas.height / 2.toFloat(),
+            canvas.width - offset.toFloat(),
+            canvas.height / 2.toFloat(),
+            paint)
+
+        val imageView = ImageView(this)
+        imageView.setImageBitmap(bitmap)
+        drawLayout.removeAllViews()
+        drawLayout.addView(imageView)
     }
 }
